28e668d1616d0c892bf64ef780398ae2e41ca732
Christian <christianlestingi33@gmail.com>
2022-11-19 16:23:18 +0100

    JSX/01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
+
 //Create a variable called hello and inizialize it with an h1 tag with the message "Hello, World!".
 
 const hello= <h1> Hello, World</h1>
-=======
-const hello = <h1> Hello, World</h1>
->>>>>>> 5dbfe601f597bcd1858340f6dea731df95b9a25f
+
