91c45d9d03a8aaa700378834cd38daa09423b76a
Christian <christianlestingi33@gmail.com>
2022-11-21 23:08:34 +0100

    props/02

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <Welcome name= 'Christian '/>
+        <Welcome />
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export class Welcome extends React.Component {
   render() {
-    return <p>Hello ,{this.props.name}</p>;
+    return <p>Hello , {this.props.name} !!</p>;
   }
+}
+Welcome.defaultProps = {
+    name : 'User'
 }
