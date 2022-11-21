3796870030ca83cf2d5fb6a814d56a51ca5a69ba
Christian <christianlestingi33@gmail.com>
2022-11-21 22:40:16 +0100

    props/01

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
 import React from "react"
 import { Hello } from "./Hello"
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
   render() {
     return (
       <div>
         <Hello />
+        <Welcome name= 'Christian '/>
       </div>
     );
   }
