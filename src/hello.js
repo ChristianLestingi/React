3a670404feec47dcd4d8a7a4cd910182681144a8
Christian <christianlestingi33@gmail.com>
2022-11-21 21:57:38 +0100

    Components/07

--- src/hello.js
+++ src/hello.js
@@ -1,13 +1,14 @@
 //Modify the Hello component so that it shows a p tag below the h1 tag with the message "What a beautiful day!".
 
 import React from "react"
+import { Message } from "./Message";
 
    export class Hello extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello, World</h1>
-        <p> What a beautiful day!</p>
+        <Message/>
       </div>
     );
   }
