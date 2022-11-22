06165a8ffc2874ea9ded0620f80eb70c5da8efeb
Christian <christianlestingi33@gmail.com>
2022-11-22 19:16:22 +0100

    props/04

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <Welcome age={30}/>
+        <Welcome  name= {<strong>Christian</strong>} age={30}/>
       </div>
     );
   }
