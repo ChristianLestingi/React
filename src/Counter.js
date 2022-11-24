69fff59509c6ff61886b29979a65a06ead0949da
Christian <christianlestingi33@gmail.com>
2022-11-24 11:15:57 +0100

    State/01

--- src/Counter.js
+++ src/Counter.js
@@ -8,7 +8,7 @@ export class Counter extends React.Component {
     super(props)
     setInterval(() => {
       this.setState((state) => {
-        return { count: state.count + 1 }
+        return { count: state.count +  1 }
       });
     }, 1000);
   }
