8448bf42412e1a9aa47ef5c1ea5335f5569f3385
Christian <christianlestingi33@gmail.com>
2022-11-24 13:14:02 +0100

    State/04

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export class CounterDisplay extends  React.Component{
     render(){
-        return this.props.count
+        return <h1>{this.props.count}</h1>
     }
 }
