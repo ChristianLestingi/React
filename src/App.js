3c591d5bed9a3b86c9d195440a377ba22c081cab
Christian <christianlestingi33@gmail.com>
2022-12-04 12:28:00 +0100

    ComponenrCompositio/01

--- src/App.js
+++ src/App.js
@@ -3,31 +3,29 @@ import { Counter } from "./Counter";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
 import { ClickCounter } from "./ClickCounter";
-import{ClickTraker} from "./ClickTracker"
+import { ClickTraker } from "./ClickTracker";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
 import { UncontrolledLogin } from "./UncontrolledLogin";
 import { TodoList } from "./TodoList";
-
-
+import { Container } from "./Container";
 
 export class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello />
-        <Welcome name="John" />
-        <Counter initialValue={10}
-                 incrementValue={1}
-                 incrementTime={3000} />
-         <ClickCounter/>
-         <ClickTraker />
-         <InteractiveWelcome/>
-         <Login/>
-         <UncontrolledLogin/>
-         <TodoList  />
+        <Container>
+          <Hello />
 
-
+          <Welcome name="John" />
+          <Counter initialValue={10} incrementValue={1} incrementTime={3000} />
+          <ClickCounter />
+          <ClickTraker />
+          <InteractiveWelcome />
+          <Login />
+          <UncontrolledLogin />
+          <TodoList />
+        </Container>
       </div>
     );
   }
