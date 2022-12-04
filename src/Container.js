eef3a126950879c9ff0e562c067a59339def9c36
Christian <christianlestingi33@gmail.com>
2022-12-04 12:45:19 +0100

    ComponentComposition/01

--- src/Container.js
+++ src/Container.js
@@ -2,7 +2,11 @@ import React from "react";
 
 export class Container extends React.Component {
   render() {
+    const Style = {
+      backgroundColor: "white",
+      border: "3px solid red",
+    };
 
-    return <div>{this.props.children}</div>;
+    return <div style={Style}>{this.props.children}</div>;
   }
 }
